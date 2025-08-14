9332ecf1cabdec00ed38e846eb958ae57139c5df
Sanjeev Ranjan <134205241+sanjeevV12@users.noreply.github.com>
2025-08-14 23:18:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ var generateName = require("sillyname");
 var sillyName = generateName();
 
 console.log(`My name is ${sillyName}.`);
+
 // `` This is back tick present in rupees Symbol
+
+// we can you import for ESM below there is the process
+// please don't excute both on same time 
+import superheroes from 'superheroes';
+const name = superheroes.random();
+
+console.log (`I am ${name}!`)
